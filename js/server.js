3ef01175c1d736a88c5a15971e33b994b430211f
anthony bell <anthonybell67.ab@gmail.com>
2024-05-26 10:54:48 -0400

    added required dependancies in the server file, started added express middleware & download nodemon so we dont have to restart our server after making changes

---
+++ js/server.js
@@ -0,0 +1,14 @@
+// declaring all of our dependancies
+const express = require('express');
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// setting up express middleware for our application
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
